6b98c3389ce7dd8b4c38cb1ce2e47b9884e70ac3
Huy Nguyen <91553769+nhathuy07@users.noreply.github.com>
2024-06-26 22:49:13 +0700

    Delete src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  GAPI_CLIENT_ID: process.env['GAPI_CLIENT_ID'],
-  // GAPI_REDIRECT_URL: "http://localhost:8100/callback",\
-  BACKEND_LOC: process.env['BACKEND_LOC'],
-  GAPI_SCOPES: process.env['GAPI_SCOPES'],
-};
